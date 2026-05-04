d04c4983c45b1af67963cb0a14f43c632f0f1ff5
Ulugbek <@ulugbek6451919-rgb>
2026-05-05 00:06:28 +0500

    final auth fix

--- server/app.js
+++ server/app.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
 
-// MongoDB ulanish
+// client papkani static qilish
+app.use(express.static(path.join(__dirname, "../client")));
+
+// MongoDB connection
 mongoose.connect(
   "mongodb://ulugbek6451919_db_user:taxi12345@ac-jtisv72-shard-00-00.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-01.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-02.uxvy0hx.mongodb.net:27017/?ssl=true&replicaSet=atlas-gg5liu-shard-0&authSource=admin&appName=Cluster0"
 )
@@ -23,7 +25,7 @@ mongoose.connect(
 
 // Home page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
 // Auth page
@@ -31,21 +33,47 @@ app.get("/auth", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/auth.html"));
 });
 
-// Register route
+// Register
 app.post("/register", async (req, res) => {
   try {
     const { name, phone, password } = req.body;
 
-    console.log("New user:");
-    console.log(name, phone, password);
+    console.log("REGISTER:", name, phone, password);
 
-    res.send("Ro‘yxatdan o‘tish muvaffaqiyatli!");
+    res.json({
+      message: "Ro‘yxatdan o‘tish muvaffaqiyatli!"
+    });
+
   } catch (err) {
-    res.send("Xatolik yuz berdi");
+    console.log(err);
+
+    res.json({
+      message: "Xatolik yuz berdi"
+    });
   }
 });
 
-// Server start
+// Login
+app.post("/login", async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+
+    console.log("LOGIN:", phone, password);
+
+    res.json({
+      message: "Login muvaffaqiyatli!"
+    });
+
+  } catch (err) {
+    console.log(err);
+
+    res.json({
+      message: "Login xato"
+    });
+  }
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
