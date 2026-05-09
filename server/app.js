3c7f85f15d377340ede21ece203108cf8e9e2ffe
Ulugbek <@ulugbek6451919-rgb>
2026-05-10 00:33:25 +0500

    full driver fix

--- server/app.js
+++ server/app.js
@@ -10,12 +10,15 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-// middleware
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static files
 app.use(express.static(path.join(__dirname, "../client")));
 
 // MongoDB
-mongoose.connect("mongodb://ulugbek6451919_db_user:taxi12345@ac-jtisv72-shard-00-00.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-01.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-02.uxvy0hx.mongodb.net:27017/?ssl=true&replicaSet=atlas-gg5liu-shard-0&authSource=admin&appName=Cluster0")
+mongoose.connect("SENING_MONGODB_URLING")
 .then(() => {
     console.log("MongoDB connected");
 })
@@ -83,21 +86,31 @@ io.on("connection", (socket) => {
     console.log("User connected");
 
     socket.on("newOrder", (order) => {
-        console.log("New Order:", order);
+        console.log("New taxi order:", order);
 
         io.emit("newOrder", order);
     });
 
     socket.on("acceptOrder", (data) => {
-        console.log("Order accepted");
-
         io.emit("orderAccepted", {
             price: data.price
         });
     });
+
+    socket.on("rideStarted", () => {
+        io.emit("rideStarted");
+    });
+
+    socket.on("rideFinished", () => {
+        io.emit("rideFinished");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
 });
 
-// START SERVER
+// SERVER START
 server.listen(PORT, () => {
     console.log(Server running on port ${PORT});
 });
