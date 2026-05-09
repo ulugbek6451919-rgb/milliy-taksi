4e22dabeb1eca54a9dcf2bd127ba36a2f6e28c29
Ulugbek <@ulugbek6451919-rgb>
2026-05-09 23:50:28 +0500

    full driver system

--- server/app.js
+++ server/app.js
@@ -1,79 +1,103 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+// middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// client papkani static qilish
 app.use(express.static(path.join(__dirname, "../client")));
 
-// MongoDB connection
-mongoose.connect(
-  "mongodb://ulugbek6451919_db_user:taxi12345@ac-jtisv72-shard-00-00.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-01.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-02.uxvy0hx.mongodb.net:27017/?ssl=true&replicaSet=atlas-gg5liu-shard-0&authSource=admin&appName=Cluster0"
-)
+// MongoDB
+mongoose.connect("mongodb://ulugbek6451919_db_user:taxi12345@ac-jtisv72-shard-00-00.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-01.uxvy0hx.mongodb.net:27017,ac-jtisv72-shard-00-02.uxvy0hx.mongodb.net:27017/?ssl=true&replicaSet=atlas-gg5liu-shard-0&authSource=admin&appName=Cluster0")
 .then(() => {
-  console.log("MongoDB connected");
+    console.log("MongoDB connected");
 })
 .catch((err) => {
-  console.log("Mongo error:", err);
+    console.log("Mongo error:", err);
 });
 
-// Home page
+// HOME
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+    res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
-// Auth page
+// AUTH
 app.get("/auth", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/auth.html"));
+    res.sendFile(path.join(__dirname, "../client/auth.html"));
 });
 
-// Register
+// DRIVER
+app.get("/driver", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/driver.html"));
+});
+
+// REGISTER
 app.post("/register", async (req, res) => {
-  try {
-    const { name, phone, password } = req.body;
+    try {
+        const { name, phone, password } = req.body;
 
-    console.log("REGISTER:", name, phone, password);
+        console.log("REGISTER:", name, phone, password);
 
-    res.json({
-      message: "Ro‘yxatdan o‘tish muvaffaqiyatli!"
-    });
+        res.json({
+            message: "Ro‘yxatdan o‘tish muvaffaqiyatli!"
+        });
 
-  } catch (err) {
-    console.log(err);
+    } catch (err) {
+        console.log(err);
 
-    res.json({
-      message: "Xatolik yuz berdi"
-    });
-  }
+        res.json({
+            message: "Xatolik yuz berdi"
+        });
+    }
 });
 
-// Login
+// LOGIN
 app.post("/login", async (req, res) => {
-  try {
-    const { phone, password } = req.body;
+    try {
+        const { phone, password } = req.body;
 
-    console.log("LOGIN:", phone, password);
+        console.log("LOGIN:", phone, password);
 
-    res.json({
-      message: "Login muvaffaqiyatli!"
-    });
+        res.json({
+            message: "Login muvaffaqiyatli!"
+        });
 
-  } catch (err) {
-    console.log(err);
+    } catch (err) {
+        console.log(err);
 
-    res.json({
-      message: "Login xato"
-    });
-  }
+        res.json({
+            message: "Xatolik yuz berdi"
+        });
+    }
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// SOCKET
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("newOrder", (order) => {
+        console.log("New Order:", order);
+
+        io.emit("newOrder", order);
+    });
+
+    socket.on("acceptOrder", (data) => {
+        console.log("Order accepted");
+
+        io.emit("orderAccepted", {
+            price: data.price
+        });
+    });
+});
+
+// START SERVER
+server.listen(PORT, () => {
+    console.log(Server running on port ${PORT});
 });
